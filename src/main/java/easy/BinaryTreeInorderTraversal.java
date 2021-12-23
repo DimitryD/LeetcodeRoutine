bb5bd996d75dc608a1b046a4346af9bdfcc5c0e6
DH <dimitrygri@gmail.com>
2021-12-23 16:59:02 +0100

    BinaryTreeInorderTraversal

---
+++ src/main/java/easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal {
+    // https://leetcode.com/problems/binary-tree-inorder-traversal/
+    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> answer = new ArrayList<>();
+//        if (root == null) return answer;
+//        if (root.left != null) answer.addAll(inorderTraversal(root.left));
+//        answer.add(root.val);
+//        if (root.right != null) answer.addAll(inorderTraversal(root.right));
+//        return answer;
+
+        List<Integer> answer = new ArrayList<>();
+        Stack<TreeNode> nodes = new Stack<>();
+        if (root == null) return answer;
+
+        TreeNode current = root;
+        nodes.push(current);
+        do {
+            if (current.left != null) {
+                nodes.push(current.left);
+                current = current.left;
+                continue;
+            }
+            current = nodes.pop();
+            answer.add(current.val);
+            if (current.right != null) {
+                nodes.push(current.right);
+                current = current.right;
+            }
+        } while (!nodes.isEmpty());
+        return answer;
+    }
+}
---
+++ src/test/java/easy/BinaryTreeInorderTraversalTest.java
@@ -0,0 +1,16 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeInorderTraversalTest {
+    BinaryTreeInorderTraversal binaryTreeInorderTraversal = new BinaryTreeInorderTraversal();
+
+    @Test
+    void test1() {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), null), null);
+        assertEquals(3, binaryTreeInorderTraversal.inorderTraversal(root).get(0));
+    }
+
+}
