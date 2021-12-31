06f5659e6df3ae9a08f85b447cf83a6e80908644
DH <dimitrygri@gmail.com>
2021-12-31 16:34:59 +0100

    TreeNode add constructor from int array

--- src/main/java/common/TreeNode.java
+++ src/main/java/common/TreeNode.java
@@ -15,4 +15,21 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    // does not support shorten representation of tree
+    public TreeNode(Integer[] nodes) {
+        this.val = nodes[0];
+        this.left = insertLevelOrder(nodes, 1); // 2 * index + 1 == 2 * 0 + 1 == 1
+        this.right = insertLevelOrder(nodes, 2); // 2 * index + 2 == 2 * 0 + 2 == 2
+    }
+
+    public TreeNode insertLevelOrder(Integer[] nodes, int index) {
+        TreeNode node = null;
+        if (index < nodes.length && nodes[index] != null) {
+            node = new TreeNode(nodes[index]);
+            node.left = insertLevelOrder(nodes, 2 * index + 1);
+            node.right = insertLevelOrder(nodes, 2 * index + 2);
+        }
+        return node;
+    }
 }
