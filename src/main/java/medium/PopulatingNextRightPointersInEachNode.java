ecc42c6851e17be45fac1129afc3e9713a3a96d3
DH <dimitrygri@gmail.com>
2022-01-04 19:13:13 +0100

    PopulatingNextRightPointersInEachNode

---
+++ src/main/java/medium/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,66 @@
+package medium;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+
+public class PopulatingNextRightPointersInEachNode {
+    // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+    public Node connect(Node root) {
+        if (root == null) return null;
+        // nodes to visit on the next level;
+        Queue<Node> nodes = new ArrayDeque<>();
+        nodes.add(root);
+        while(!nodes.isEmpty()) {
+            List<Node> levelNodes = new ArrayList<>(nodes);
+            nodes.clear();
+            Node prevNode = levelNodes.get(0);
+            for (Node node: levelNodes) {
+                if (node != prevNode) {
+                    prevNode.next = node;
+                    prevNode = node;
+                }
+                if (node.left != null) nodes.add(node.left);
+                if (node.right != null) nodes.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public Node connectRecursive(Node root) {
+        if (root == null || root.left == null) return root;
+        point(root.left, root.right, null);
+        return root;
+    }
+
+
+    private void point(Node left, Node right, Node parentRight) {
+        left.next = right;
+        if (parentRight != null)
+            right.next = parentRight.left;
+        if (left.left != null) {
+            point(left.left, left.right, right);
+            point(right.left, right.right, right.next);
+        }
+    }
+}
---
+++ src/test/java/medium/PopulatingNextRightPointersInEachNodeTest.java
@@ -0,0 +1,22 @@
+package medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PopulatingNextRightPointersInEachNodeTest {
+    PopulatingNextRightPointersInEachNode populatingNextRightPointersInEachNode = new PopulatingNextRightPointersInEachNode();
+
+    @Test
+    void test1() {
+        Node root = new Node(1, new Node(2), new Node(3), null);
+        assertEquals(3, populatingNextRightPointersInEachNode.connect(root).left.next.val);
+    }
+
+    @Test
+    void test2() {
+        Node root = new Node(1, new Node(2), new Node(3), null);
+        assertEquals(3, populatingNextRightPointersInEachNode.connectRecursive(root).left.next.val);
+    }
+
+}
