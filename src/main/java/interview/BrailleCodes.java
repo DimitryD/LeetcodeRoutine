1ea9463895cce70e3b66d5efb9e04c00c44dc368
DH <dimitrygri@gmail.com>
2022-02-06 12:01:34 +0100

    BrailleCodes

---
+++ src/main/java/interview/BrailleCodes.java
@@ -0,0 +1,56 @@
+package interview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class BrailleCodes {
+    private static Map<Character, String> alphabet;
+    public static String solution(String s) {
+        // Set up alphabet
+        if (alphabet == null) setUpAlphabet();
+        StringBuilder encodedString = new StringBuilder();
+        for (char c: s.toCharArray())
+            // if capital letter faced (based on ASCI   I)
+            if (c >= 65  && c <= 90) {
+                // '!' means Braille capitalization mark
+                encodedString.append(alphabet.get('!'));
+                // based on ASCII table
+                c += 32;
+                encodedString.append(alphabet.get(c));
+            } else encodedString.append(alphabet.get(c));
+        return encodedString.toString();
+    }
+
+    private static void setUpAlphabet() {
+        alphabet = new HashMap<>();
+        alphabet.put('a', "100000");
+        alphabet.put('b', "110000");
+        alphabet.put('c', "100100");
+        alphabet.put('d', "100110");
+        alphabet.put('e', "100010");
+        alphabet.put('f', "110100");
+        alphabet.put('g', "110110");
+        alphabet.put('h', "110010");
+        alphabet.put('i', "010100");
+        alphabet.put('j', "010110");
+        alphabet.put('k', "101000");
+        alphabet.put('l', "111000");
+        alphabet.put('m', "101100");
+        alphabet.put('n', "101110");
+        alphabet.put('o', "101010");
+        alphabet.put('p', "111100");
+        alphabet.put('q', "111110");
+        alphabet.put('r', "111010");
+        alphabet.put('s', "011100");
+        alphabet.put('t', "011110");
+        alphabet.put('u', "101001");
+        alphabet.put('v', "111001");
+        alphabet.put('w', "010111");
+        alphabet.put('x', "101101");
+        alphabet.put('y', "101111");
+        alphabet.put('z', "101011");
+        alphabet.put(' ', "000000");
+        alphabet.put('!', "000001");
+    }
+}
---
+++ src/test/java/interview/BrailleCodesTest.java
@@ -0,0 +1,37 @@
+package interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrailleCodesTest {
+
+    @Test
+    void test1() {
+        assertEquals("100100101010100110100010", BrailleCodes.solution("code"));
+    }
+
+    @Test
+    void test2() {
+        assertEquals("000001110000111010100000010100111000111000100010", BrailleCodes.solution("Braille"));
+    }
+
+    @Test
+    void test3() {
+        assertEquals("000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110",
+                BrailleCodes.solution("The quick brown fox jumps over the lazy dog"));
+    }
+
+    @Test
+    void test4() {
+        assertEquals("000001011110110010100010",
+                BrailleCodes.solution("The"));
+    }
+
+    @Test
+    void test5() {
+        assertEquals("110010",
+                BrailleCodes.solution("h"));
+    }
+
+}
