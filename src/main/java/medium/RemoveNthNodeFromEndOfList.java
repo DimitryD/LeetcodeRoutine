17b8fcd06871a3a6e904dd932301aa774cb4bc9a
DH <dimitrygri@gmail.com>
2022-02-16 20:59:06 +0100

    ListNode refactor

--- src/main/java/medium/RemoveNthNodeFromEndOfList.java
+++ src/main/java/medium/RemoveNthNodeFromEndOfList.java
@@ -1,12 +1,7 @@
 package medium;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+
+import common.ListNode;
 
 public class RemoveNthNodeFromEndOfList {
     // https://leetcode.com/problems/remove-nth-node-from-end-of-list/
--- src/test/java/medium/RemoveNthNodeFromEndOfListTest.java
+++ src/test/java/medium/RemoveNthNodeFromEndOfListTest.java
@@ -1,5 +1,6 @@
 package medium;
 
+import common.ListNode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
